309ac139dea75980548e7e3982efb8cabf3f66e0
hafsa-saeed <hafsasaeed1074@gmail.com>
2026-05-13 00:34:07 -0700

    Updated server.js and port settings

--- backend/server.js
+++ backend/server.js
@@ -66,7 +66,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅  MongoDB connected");
-    app.listen(PORT, () =>
+    app.listen(PORT, "0.0.0.0", () =>
       console.log(`🚀  FlavorCraft API → http://localhost:${PORT}`),
     );
   })
